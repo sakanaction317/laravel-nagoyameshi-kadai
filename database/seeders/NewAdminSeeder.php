77177423167cda225e8f903a6336411031dfbadb
sakanaction317 <danki.karate9602@gmail.com>
2024-11-14 23:03:02 +0900

    add newadmin to seeder

--- database/seeders/NewAdminSeeder.php
+++ database/seeders/NewAdminSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Admin;
+use Illuminate\Support\Facades\Hash;
 
 class NewAdminSeeder extends Seeder
 {
