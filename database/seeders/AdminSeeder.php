ec43419e90a631d9ff1a4f49b7e625b7bdabbdf0
sakanaction317 <danki.karate9602@gmail.com>
2024-11-20 01:57:44 +0900

    管理者側の店舗管理機能を実装

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Admin;
+use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
 class AdminSeeder extends Seeder
@@ -14,9 +15,27 @@ class AdminSeeder extends Seeder
      */
     public function run(): void
     {
-        $admin = new Admin();
-        $admin->email = 'admin@example.com';
-        $admin->password = Hash::make('nagoyameshi');
-        $admin->save();
+        if (!Admin::where('email', 'admin@example.com')->exists()) {
+            $admin = new Admin();
+            $admin->email = 'admin@example.com';
+            $admin->password = Hash::make('nagoyameshi');
+            $admin->save();
+        }
+
+        // 新しいアカウント
+        Admin::create([
+            'email' => 'admin_review@example.com',
+            'password' => Hash::make('admin_password'),
+        ]);
+
+        User::create([
+            'name' => '例 太郎',
+            'kana' => 'レイ タロウ',
+            'email' => 'user_review@example.com',
+            'password' => Hash::make('user_password'),
+            'postal_code' => '2222222',
+            'address' => '兵庫県神戸市',
+            'phone_number' => '09011111111',
+        ]);
     }
 }
