b042fa813f0319fa811fc115f8c7dd6136b6321e
sakanaction317 <danki.karate9602@gmail.com>
2024-11-30 13:08:26 +0900

    レビュー機能を修正

--- database/migrations/2024_11_28_181710_create_reviews_table.php
+++ database/migrations/2024_11_28_181710_create_reviews_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->text('content');
             $table->integer('score')->unsigned();
             $table->foreignId('restaurant_id')->constrained()->cascadeOnDelete();
-            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
             $table->timestamps();
         });
     }
