04a5e49b17168a5bfdb72ba909ac3781ec4cfe8b
sakanaction317 <danki.karate9602@gmail.com>
2024-12-02 21:30:28 +0900

    お気に入り機能修正

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\RestaurantController;
 use App\Http\Controllers\SubscriptionController;
 use App\Http\Controllers\ReviewController;
 use App\Http\Controllers\ReservationController;
-use App\http\Controllers\FavoriteController;
+use App\Http\Controllers\FavoriteController;
 use App\Http\Controllers\Admin\HomeController as AdminHomeController;
 use App\Http\Controllers\Admin\UserController as AdminUserController;
 use App\Http\Controllers\Admin\RestaurantController as AdminRestaurantController;
@@ -54,16 +54,17 @@ Route::get('users/{user}', [Admin\UserController::class, 'show'])->name('users.s
 Route::group(['middleware' => 'guest:admin'], function () {
     Route::get('/', [HomeController::class, 'index'])->name('home');
     Route::resource('restaurants', RestaurantController::class)->only(['index', 'show']);
-   
+    
+
+
 
     /*ログイン済*/
     Route::group(['middleware' => ['auth']], function () {
         Route::resource('user', UserController::class)->only(['index', 'edit', 'update']);
         Route::resource('restaurants.reviews', ReviewController::class)->only(['index']);
 
-
-        // 有料会員
-        Route::group(['middleware' => [Subscribed::class]], function(){
+        /*有料会員*/
+        Route::group(['middleware' => [Subscribed::class]], function () {
             Route::get('subscription/edit', [SubscriptionController::class, 'edit'])->name('subscription.edit');
             Route::patch('subscription', [SubscriptionController::class, 'update'])->name('subscription.update');
             Route::get('subscription/cancel', [SubscriptionController::class, 'cancel'])->name('subscription.cancel');
@@ -71,12 +72,14 @@ Route::group(['middleware' => 'guest:admin'], function () {
             Route::resource('restaurants.reviews', ReviewController::class)->only(['create', 'store', 'edit', 'update', 'destroy']);
             Route::resource('restaurants.reservations', ReservationController::class)->only(['create', 'store']);
             Route::resource('reservations', ReservationController::class)->only(['index', 'destroy']);
+            /*Route::resource('favorites', FavoriteController::class)->only(['index', 'store', 'destroy']);*/
             Route::get('favorites', [FavoriteController::class, 'index'])->name('favorites.index');
-            Route::post('favorites/{restaurant}', [FavoriteController::class, 'store'])->name('favorites.store');
-            Route::delete('favorites/{restaurant}', [FavoriteController::class, 'destroy'])->name('favorites.destroy');
+            Route::post('favorites/{restaurant_id}', [FavoriteController::class, 'store'])->name('favorites.store');
+            Route::delete('favorites/{restaurant_id}', [FavoriteController::class, 'destroy'])->name('favorites.destroy');
         });
 
-        Route::group(['middleware' => [NotSubscribed::class]], function(){
+        /*無料会員*/
+        Route::group(['middleware' => [NotSubscribed::class]], function () {
             Route::get('subscription/create', [SubscriptionController::class, 'create'])->name('subscription.create');
             Route::post('subscription', [SubscriptionController::class, 'store'])->name('subscription.store');
         });
